26369ab31b8697420e4859165b4a2a24124a1bd5
dpodbereznyh <dpodbereznyh@gmail.com>
2020-09-01 20:30:37 +0500

    Финал

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 70){
+        if ($(this).scrollTop() > 90){
             $('header').addClass("sticky");
         }
         else{
