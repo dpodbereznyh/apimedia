83dd0be1a659be3e9b1f53b1c1125618bb6b9e48
dpodbereznyh <dpodbereznyh@gmail.com>
2020-09-01 18:00:01 +0500

    На проверку

--- app/js/common.js
+++ app/js/common.js
@@ -38,6 +38,7 @@ $(document).ready(function () {
         $('body,html').animate({scrollTop: top}, 500);
     });
 
+
     $(".js-callback").on( "click", function() {
         var newtitle = $(this).attr("data-title");
         var newinput = $(this).attr("data-input");
@@ -102,4 +103,71 @@ var certificatesSlider = new Swiper ('.certificates__swiper-container', {
     wrapperClass: 'certificates__swiper-wrapper',
     slidesPerView: 2,
     spaceBetween: 16,
+    navigation: {
+        nextEl: '.certificates__slider_button-next',
+        prevEl: '.certificates__slider_button-prev',
+    }
 });
+
+new WOW().init();
+
+var ymID = 66898711;
+
+$(function() {
+    $('.js-callback').on("click", function() {
+        var sendPopup = $(this).attr('data-send');
+        // $(modal).find('input[name=target]').val(parent);
+        $(".popup-form__btn").attr("data-goal", sendPopup);
+        console.log(sendPopup);
+    });
+
+});
+
+var metrikaGoals = function () {
+
+
+    $('.js-onClickGoal').click(function () {
+
+        var goal = this.dataset.goal;
+        // var gtagGoal = this.dataset.gtagGoal;
+        // var thisGoal = getCookie(goal);
+        if (typeof thisGoal == "undefined") {
+            if ((typeof goal !== 'undefined' || goal != '') && typeof ym !== 'undefined') {
+                ym(ymID, 'reachGoal', goal);
+            }
+
+            var date = new Date(new Date().getTime() + 720 * 3600 * 1000);
+            document.cookie = goal + "=1; path=/; expires=" + date.toUTCString();
+        }
+
+
+    });
+
+    $('.js-validate-form').click(function () {
+        var form = $(this).closest('.js-form-default');
+        var validated = 1;
+
+        $(form).find('input[required]').each(function (indexInArray, valueOfElement) {
+            if ($(valueOfElement).val() == "") {
+                validated = 0;
+            }
+        });
+
+        if (validated == 0) {
+
+
+            var goal = this.dataset.goal;
+            // var gtagGoal = this.dataset.gtagGoal;
+
+            if ((typeof goal !== 'undefined' || goal != '') && typeof ym !== 'undefined') {
+                ym(ymID, 'reachGoal', goal);
+            }
+        }
+        console.log(form);
+    });
+};
+
+$(document).ready(function () {
+    metrikaGoals();
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,7 @@ gulp.task('scripts', function() {
         'app/js/vendors/jquery.smartmenus.js',
         'app/js/vendors/swiper.min.js',
         'app/js/vendors/jquery.fancybox.min.js',
+        'app/js/vendors/wow.js',
         'app/js/common.js', // Always at the end
     ])
         .pipe(concat('scripts.min.js'))
